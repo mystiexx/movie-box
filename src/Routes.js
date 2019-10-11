0cb1671d4c5c4e7e77f2462cad1c753d580d4c69
Roland Enola <40168844+mystiexx@users.noreply.github.com>
2019-10-11 20:00:24 +0100

    update

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import App from './App'
 import TvShows from './components/TvTab/TvShows'
 import Header from './components/HeaderTab/Header'
+import ShowMovies from './components/MoviesTab/ShowMovies'
 
 class Routes extends Component{
     render(){
@@ -14,6 +15,7 @@ class Routes extends Component{
                
                 <Route exact component={App} path='/'/>
                 <Route exact component={TvShows} path='/tvshows'/>
+                <Route exact component={ShowMovies} path='/movies/:id'/>
             
                 </Switch>
             </div>
---
+++ src/components/MoviesTab/ShowMovies.js
@@ -0,0 +1,144 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import { Container, Row, Col, Card, Badge } from 'react-bootstrap'
+import { FaStar } from 'react-icons/fa'
+import Reviews from './Reviews'
+
+class ShowMovies extends Component {
+    constructor() {
+        super()
+        this.state = {
+            movies: [],
+            reviews: []
+        }
+
+    }
+
+    componentWillMount() {
+        const { match } = this.props
+        axios.get(`https://api.themoviedb.org/3/movie/${match.params.id}?api_key=d0ff6f9cb6c33dcc6bc0c0a6381f2884&language=en-US`)
+            .then(res => {
+                const movies = [res.data];
+                this.setState({ movies })
+            })
+    }
+
+    componentDidMount(){
+        const {match} = this.props
+        axios.get(`https://api.themoviedb.org/3/movie/${match.params.id}/reviews?api_key=d0ff6f9cb6c33dcc6bc0c0a6381f2884&language=en-US&page=1`)
+        .then(res=> {
+            const reviews = [res.data];
+            this.setState({ reviews })
+        })
+    }
+    render() {
+        const { movies, reviews } = this.state
+        console.log(reviews)
+        return (
+            <div>
+
+                <Container className="mt-5 pb-5">
+                    <Card className="shadow" border="light">
+                        {movies.map((movies, i) =>
+                            <Row>
+                                <Col md={2} key={i}>
+                                    <img src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movies.poster_path}`} className="poster-image" alt="poster" />
+                                </Col>
+                                <Col>
+                                    <Card.Body>
+                                        <h1>{movies.original_title}</h1>
+                                        <h6>Rating: {movies.vote_average}<FaStar /></h6>
+                                        <h6>{movies.tagline}</h6>
+                                        <Card.Text>
+                                            <h6 className="mt-2">Overview</h6>
+                                            {movies.overview}
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Col>
+                            </Row>
+                        )}
+                    </Card>
+
+
+
+                    <Card className="mt-5 shadow">
+                        <Row>
+                            <Col   className="p-2">
+                                {
+                                   reviews.map((data, i)=> {
+                                       return(
+                                           <Reviews data={data.results} key={i}/>
+                                       )
+                                   })
+                                }
+                               
+
+                            </Col>
+
+                            <Col className="details" md={2}>
+                                {movies.map((data, i) =>
+                                    <div className="p-3" key={i}>
+                                        <h6>Facts</h6>
+                                        <h6>Status</h6>
+                                        <p>{data.status}</p>
+
+                                        <h6>RunTime</h6>
+                                        <p>{data.runtime} minutes</p>
+
+                                        <h6>Budget</h6>
+                                        <p>${data.budget.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+
+                                        <h6>Revenue</h6>
+                                        <p>${data.revenue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+
+
+
+                                        <h6>Original Language</h6>
+                                        {
+                                            data.spoken_languages.map(data =>
+                                                <Card.Text key={data.id}>{data.name} </Card.Text>
+                                            )}
+
+                                        <h6>Genres</h6>
+                                       
+                                        {data.genres.map(data=>
+                                         <div className="m-2">
+                                            <Badge variant="dark" key={data.id}>{data.name}</Badge>
+                                            </div>
+                                            )}
+
+                                            <h6>Production Companies</h6>
+                                            {
+                                                data.production_companies.map(data=>{
+                                                    return(
+                                                        <div key={data.id}>
+                                                    
+                                                                <Card.Text>
+                                                                {data.name}
+                                                                </Card.Text>
+                                                           
+                                                            </div>
+                                                    )
+                                                })
+                                            }
+                                            
+                                    </div>
+                                )}
+                            </Col>
+                        </Row>
+
+                    </Card>
+
+
+                </Container>
+
+
+
+
+
+            </div>
+        )
+    }
+}
+
+export default ShowMovies;
